50f6d2bc0304a7cfc25538217024c9f9fab6074e
danielkrata09 <danielkrata09@gmail.com>
2014-05-05 21:34:32 +0200

    Stworzone szablony, Dodane packages, Stworzone projekty do testów

---
+++ TimeTracker/TimeTracker.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeTracker.Web.Startup))]
+namespace TimeTracker.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
