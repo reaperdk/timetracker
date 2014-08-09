27825570c8855d326db4ebfe866ba2d42da34d81
nicin <michalnicinski@gmail.com>
2014-08-09 12:08:48 +0200

    Repository project added

---
+++ TimeTracker/TimeTracker.Repository/EntitiesRepository.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using TimeTracker.Model;
+
+namespace TimeTracker.Repository
+{
+    public class EntitiesRepository<T> : IEntitiesRepository<T> where T : class
+    {
+        readonly EntitiesContext _context;
+
+        bool _disposed;
+
+        public EntitiesRepository()
+        {
+            _context = new EntitiesContext();
+            _disposed = false;
+        }
+
+        public T Get(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public IQueryable<T> Get()
+        {
+            return _context.Set<T>();
+        }
+
+        public IQueryable<T> Get(Expression<Func<T, bool>> predicate)
+        {
+            return _context.Set<T>().Where(predicate);
+        }
+
+        public async Task<T> GetAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void Insert(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Remove(int id)
+        {
+            T entity = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ TimeTracker/TimeTracker.Repository/IEntitiesRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Repository
+{
+    public interface IEntitiesRepository<T> : IDisposable where T : class
+    {
+        T Get(int id);
+        IQueryable<T> Get();
+        IQueryable<T> Get(Expression<Func<T, bool>> predicate);
+        Task<T> GetAsync(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Remove(int id);
+        void Save();
+        Task SaveAsync();
+    }
+}
