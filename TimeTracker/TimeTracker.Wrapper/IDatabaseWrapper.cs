8c06812becc673f6deef078b24e71f0aa2da71d9
nicin <michalnicinski@gmail.com>
2014-09-01 22:43:39 +0200

    IDatabaseWrapper - common interface for ClassService, WCF and WebApi

---
+++ TimeTracker/TimeTracker.Wrapper/IDatabaseWrapper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.Model;
+
+namespace TimeTracker.Wrapper
+{
+    public interface IDatabaseWrapper<T> where T : class, IModelBase
+    {
+        /// <summary>
+        /// Gets T-type entity by its id from database
+        /// </summary>
+        /// <param name="id">Id of T model</param>
+        /// <returns>T model if found, otherwise null</returns>
+        /// <exception cref="ArgumenException">Thrown when id is less or equal zero</exception>
+        T Get(int id);
+        TResult Get<TResult>(int id) where TResult : class, IModelBase;
+
+        /// <summary>
+        /// Gets list of entities of T-type from database
+        /// </summary>
+        /// <returns>List of T-type entities if found, otherwise empty list</returns>
+        IEnumerable<T> Get();
+        IEnumerable<TResult> Get<TResult>() where TResult : class, IModelBase;
+
+        ///// <summary>
+        ///// Gets list of entities of T-type from database via WebApi with a specified http request
+        ///// </summary>
+        ///// <param name="httpRequest">Specified http request</param>
+        ///// <returns>List of T-type entities if found, otherwise empty list</returns>
+        ///// <exception cref="ArgumentException">Thrown when httpRequest is null or empty</exception>
+        //IEnumerable<T> Get(string httpRequest);
+        //IEnumerable<TResult> Get<TResult>(string httpRequest) where TResult : class, IModelBase;
+
+        /// <summary>
+        /// Adds new entity to database
+        /// </summary>
+        /// <param name="entity">Entity to add to database</param>
+        /// <returns>Returns entity with id if success, otherwise null</returns>
+        /// <exception cref="ArgumentException">Thrown when entity is null</exception>
+        T Add(T entity);
+
+        ///// <summary>
+        ///// Adds new entity to database via WebApi with a specified http request 
+        ///// </summary>
+        ///// <param name="httpRequest">Specified http request</param>
+        ///// <param name="data">Entity to add to database</param>
+        ///// <exception cref="ArgumentNullException">Thrown when data is null</exception>
+        ///// <exception cref="ArgumentException">Thrown when httpRequest is null or empty</exception>
+        //void Add(string httpRequest, object data);
+
+        /// <summary>
+        /// Updates entity in database
+        /// </summary>
+        /// <param name="entity">Entity to update</param>
+        /// <returns>T-type entity if updated, otherwise null</returns>
+        /// <exception cref="ArgumentNullException">Thrown when entity is null</exception>
+        T Update(T entity);
+
+        /// <summary>
+        /// Deletes T-type entity by its id from database
+        /// </summary>
+        /// <param name="id">Id of entity</param>
+        /// <exception cref="ArgumentException">Thrown when id is less or equal zero</exception>
+        void Delete(int id);
+    }
+}
+    
