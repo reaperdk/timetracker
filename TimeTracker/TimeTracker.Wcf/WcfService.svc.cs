0796bb85a29f8aebf379124bed4e628f211b5b45
danielkrata09 <danielkrata09@gmail.com>
2014-09-06 15:39:12 +0200

    Renamed WcfService - derive from IWcfContract. Deleted unnecesary IService1. Updated project and solution

---
+++ TimeTracker/TimeTracker.Wcf/WcfService.svc.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using TimeTracker.Model;
+using TimeTracker.WcfContract;
+
+namespace TimeTracker.Wcf
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class WcfService : IWcfContract
+    {
+        public IEnumerable<UserProfile> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserProfile GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateUser(UserProfile user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(UserProfile user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitializeRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<webpages_Roles> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public webpages_Roles GetRoleById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
