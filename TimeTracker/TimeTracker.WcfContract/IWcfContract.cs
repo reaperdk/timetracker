bfdc1f78b065af98e879521e665acc05da27f979
danielkrata09 <danielkrata09@gmail.com>
2014-09-06 15:37:03 +0200

    Created WcfConstract

---
+++ TimeTracker/TimeTracker.WcfContract/IWcfContract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using TimeTracker.Model;
+
+namespace TimeTracker.WcfContract
+{
+    [ServiceContract(SessionMode = SessionMode.Allowed)]
+    public interface IWcfContract
+    {
+        [OperationContract]
+        IEnumerable<UserProfile> GetAllUsers();
+        [OperationContract]
+        UserProfile GetUserById(int id);
+        [OperationContract]
+        void CreateUser(UserProfile user);
+        [OperationContract]
+        void UpdateUser(UserProfile user);
+        [OperationContract]
+        void RemoveUser(int id);
+        [OperationContract]
+        void InitializeRoles();
+        [OperationContract]
+        IEnumerable<webpages_Roles> GetAllRoles();
+        [OperationContract]
+        webpages_Roles GetRoleById(int id);
+    }
+}
