c73f2f8b9b56f61274928f213842a9e701187294
danielkrata09 <danielkrata09@gmail.com>
2014-09-06 16:19:50 +0200

    Created WcfWrapper

---
+++ TimeTracker/TimeTracker.Wrapper/WcfWrapper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.Wrapper.WcfWrapperConnection;
+
+namespace TimeTracker.Wrapper
+{
+    public class WcfWrapper : IServiceWrapper
+    {
+        private readonly IWcfConnectionService _connectionService;
+
+        public WcfWrapper()
+        {
+            _connectionService = new WcfConnectionService();
+        }
+        public WcfWrapper(IWcfConnectionService connectionService)
+        {
+            
+        }
+        public IEnumerable<Model.UserProfile> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.UserProfile GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateUser(Model.UserProfile user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser(Model.UserProfile user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitializeRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Model.webpages_Roles> GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Model.webpages_Roles GetRoleById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
