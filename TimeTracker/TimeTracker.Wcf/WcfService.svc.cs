6110ff255dda8b0ceaccb0424acf3259124d08bc
danielkrata09 <danielkrata09@gmail.com>
2014-09-06 16:17:24 +0200

    Added private class members. Added implementation of GetAllUsers().

--- TimeTracker/TimeTracker.Wcf/WcfService.svc.cs
+++ TimeTracker/TimeTracker.Wcf/WcfService.svc.cs
@@ -7,15 +7,26 @@ using System.ServiceModel.Web;
 using System.Text;
 using TimeTracker.Model;
 using TimeTracker.WcfContract;
+using TimeTracker.ClassService;
 
 namespace TimeTracker.Wcf
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class WcfService : IWcfContract
     {
+        //These service classes will be used to connect to database and do necessary things
+        private readonly IUsersService _usersService;
+        private readonly IRolesService _rolesService;
+
+        public WcfService(IUsersService usersService, IRolesService rolesService)
+	    {
+            _usersService = usersService;
+            _rolesService = rolesService;
+	    }
+
         public IEnumerable<UserProfile> GetAllUsers()
         {
-            throw new NotImplementedException();
+            return _usersService.GetAll();
         }
 
         public UserProfile GetUserById(int id)
