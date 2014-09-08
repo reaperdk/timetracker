d78f060cbe43bc94cb40cc2333b0e9d65e9999bc
danielkrata09 <danielkrata09@gmail.com>
2014-09-08 22:29:17 +0200

    Created TestFixtureSetUp for UsersServiceTest

---
+++ TimeTracker/ClassServiceTests/UsersServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using TimeTracker.Model;
+using TimeTracker.Repository;
+using TimeTracker.ClassService;
+
+namespace ClassServiceTests
+{
+    [TestFixture]
+    public class UsersServiceTest
+    {
+        private IUsersService _usersService;
+        private IEntitiesRepository<UserProfile> _fakeRepo;
+
+        [TestFixtureSetUp]
+        public void StartUp()
+        {
+            _fakeRepo = Substitute.For<IEntitiesRepository<UserProfile>>();
+            Func<IEntitiesRepository<UserProfile>> fakeGetRepository = () => fakeRepo;
+
+            _usersService = new UsersService(fakeGetRepository);
+        }
+
+        [Test]
+        public void IsReturningUserByIdIfUserExist()
+        {
+        }
+    }
+}
