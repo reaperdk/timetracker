92b8ef15387d0305de0dec66ee97e20f388b7f13
danielkrata09 <danielkrata09@gmail.com>
2014-09-09 21:45:13 +0200

    Created first real test for UsersService

--- TimeTracker/ClassServiceTests/UsersServiceTest.cs
+++ TimeTracker/ClassServiceTests/UsersServiceTest.cs
@@ -1,30 +1,64 @@
 ﻿using System;
+using System.Linq;
 using NSubstitute;
 using NUnit.Framework;
+using Moq;
 using TimeTracker.Model;
 using TimeTracker.Repository;
 using TimeTracker.ClassService;
+using System.Linq.Expressions;
 
 namespace ClassServiceTests
 {
     [TestFixture]
     public class UsersServiceTest
     {
-        private IUsersService _usersService;
-        private IEntitiesRepository<UserProfile> _fakeRepo;
+        IQueryable<UserProfile> _userProfilesWithRoles;
+        IQueryable<UserProfile> _userProfilesWitouthRoles;
 
         [TestFixtureSetUp]
         public void StartUp()
         {
-            _fakeRepo = Substitute.For<IEntitiesRepository<UserProfile>>();
-            Func<IEntitiesRepository<UserProfile>> fakeGetRepository = () => _fakeRepo;
+            _userProfilesWithRoles = new [] 
+            {
+                new UserProfile { UserId = 1 },
+                new UserProfile { UserId = 2 }
+            }.AsQueryable();
 
-            _usersService = new UsersService(fakeGetRepository);
+            _userProfilesWitouthRoles = new[] 
+            {
+                new UserProfile 
+                { 
+                    UserId = 1,  
+                    webpages_Roles = new [] 
+                    {
+                        new RoleModel { RoleId = 1 }
+                    }
+                },
+                new UserProfile
+                { 
+                    UserId = 2,  
+                    webpages_Roles = new [] 
+                    {
+                        new RoleModel { RoleId = 2 }
+                    }
+                }
+            }.AsQueryable();
         }
 
         [Test]
         public void IsReturningUserByIdIfUserExist()
         {
+            var mock = Substitute.For<IEntitiesRepository<UserProfile>>();
+            mock.Get(It.IsAny<Expression<Func<UserProfile, RoleModel>>>()).Returns(_userProfilesWithRoles);
+
+            Func<IEntitiesRepository<UserProfile>> getFakeRepository = () => mock;
+            //Subject of Unit Test
+            var sut = new UsersService(getFakeRepository);
+
+            var result = sut.GetAll();
+
+            Assert.That(sut.GetAll().Count(), Is.EqualTo(2));
         }
     }
 }
