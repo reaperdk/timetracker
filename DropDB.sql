4cfffc42d02400254d83712f1d654bf57f1b4274
nicin <michalnicinski@gmail.com>
2014-09-22 17:42:31 +0200

    DropDB script update

--- DropDB.sql
+++ DropDB.sql
@@ -1,25 +1,29 @@
 ﻿IF OBJECT_ID('__MigrationHistory', 'U') IS NOT NULL
   DROP TABLE __MigrationHistory;
-IF OBJECT_ID('SlotModels', 'U') IS NOT NULL
-  DROP TABLE SlotModels;
+IF OBJECT_ID('Slots', 'U') IS NOT NULL
+  DROP TABLE Slots;
 IF OBJECT_ID('UserProfileProjectModels', 'U') IS NOT NULL
   DROP TABLE UserProfileProjectModels;
 IF OBJECT_ID('ProjectModelUserProfiles', 'U') IS NOT NULL
   DROP TABLE ProjectModelUserProfiles;
-IF OBJECT_ID('TaskModels', 'U') IS NOT NULL
-  DROP TABLE TaskModels;
-IF OBJECT_ID('ProjectModels', 'U') IS NOT NULL
-  DROP TABLE ProjectModels;
-IF OBJECT_ID('UserModels', 'U') IS NOT NULL
-  DROP TABLE UserModels;
-IF OBJECT_ID('StatusModels', 'U') IS NOT NULL
-  DROP TABLE StatusModels;
-IF OBJECT_ID('PriorityModels', 'U') IS NOT NULL
-  DROP TABLE PriorityModels;
-IF OBJECT_ID('TypeModels', 'U') IS NOT NULL
-  DROP TABLE TypeModels;
-IF OBJECT_ID('CategoryModels', 'U') IS NOT NULL
-  DROP TABLE CategoryModels;
+IF OBJECT_ID('ProjectsUsers', 'U') IS NOT NULL
+  DROP TABLE ProjectsUsers;
+  IF OBJECT_ID('UsersProjects', 'U') IS NOT NULL
+  DROP TABLE UsersProjects;
+IF OBJECT_ID('Tasks', 'U') IS NOT NULL
+  DROP TABLE Tasks;
+IF OBJECT_ID('Projects', 'U') IS NOT NULL
+  DROP TABLE Projects;
+IF OBJECT_ID('Users', 'U') IS NOT NULL
+  DROP TABLE Users;
+IF OBJECT_ID('Statuses', 'U') IS NOT NULL
+  DROP TABLE Statuses;
+IF OBJECT_ID('Priorities', 'U') IS NOT NULL
+  DROP TABLE Priorities;
+IF OBJECT_ID('Types', 'U') IS NOT NULL
+  DROP TABLE Types;
+IF OBJECT_ID('Categories', 'U') IS NOT NULL
+  DROP TABLE Categories;
 IF OBJECT_ID('webpages_UsersInRoles', 'U') IS NOT NULL
   DROP TABLE webpages_UsersInRoles;
 IF OBJECT_ID('webpages_RolesUserProfile', 'U') IS NOT NULL
